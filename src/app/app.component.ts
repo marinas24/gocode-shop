8848513af148024c7735e31ae37fdd3a2111b80f
Marina Berger <marinas24@gmail.com>
2021-01-26 21:21:58 +0200

    ex2-creating dynamic products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PRODUCTS } from './products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'gocode-shop';
-  text = "";
+ 
+ // title = 'gocode-shop';
+  //text = "";
   
-  onBtnClick(): void {
-    this.text = this.text ? '' : this.text = "Good job"
+  //onBtnClick(): void {
+  //  this.text = this.text ? '' : this.text = "Good job"
+  products = PRODUCTS;
   }
-}
+
 
